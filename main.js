5f1679eb8b37672da94adcc668147b7610a1a77e
Trammell Hudson <hudson@trmm.net>
2022-01-15 16:00:33 +0100

    wire up some javascript, keys fill in the boxes

--- main.js
+++ main.js
@@ -1,10 +1,68 @@
 // get all the keyboard keys and attach click event listeners
-const keys = document.getElementByClassName('key');
+"use strict";
 
-for (let i = 0; i < keys.length; i++) {
-	keys[i].addEventListener('click', place_letter.bind(keys[i], i));
+for(let k of document.querySelectorAll('.key'))
+	k.addEventListener('click', (e) => place_letter(k,e))
+
+const rows = [];
+let guess_row = 0;
+let guess_col = 0;
+
+function place_letter(k,e) {
+	const key = k.id;
+	console.log(key);
+	e.preventDefault();
+
+	if (key == 'delete')
+	{
+		if (guess_col == 0)
+			return;
+
+		guess_col--;
+		rows[guess_row][guess_col].classList.remove('visible');
+		return;
+	}
+
+	if (key == 'enter')
+	{
+		// todo: actually check it
+		if (guess_row == 6)
+			return;
+		guess_col = 0;
+		guess_row++;
+		return;
+	}
+
+	if(guess_col == 5)
+	{
+		// too many!
+		return;
+	}
+
+	const sq = rows[guess_row][guess_col++];
+	sq.innerText = key;
+	sq.classList.add('visible');
 }
 
-function place_letter() {
-	
+// create the guess rows
+const rows_div = document.getElementById('rows');
+for(let i = 0 ; i < 6 ; i++)
+{
+	const row = document.createElement('div');
+	row.classList.add('row');
+
+	rows[i] = [];
+
+	for(let j = 0 ; j < 5 ; j++)
+	{
+		const e = document.createElement('span');
+		e.classList.add('guessbox');
+		e.id = i + "," + j;
+		e.innerText = '0';
+		row.appendChild(e);
+		rows[i].push(e);
+	}
+
+	rows_div.appendChild(row);
 }
+
